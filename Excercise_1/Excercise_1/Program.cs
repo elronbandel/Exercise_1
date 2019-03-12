12cb538bc6a09f923e00070fa60c2fde9d5652ab
elron <elronbandel@gmail.com>
2019-03-12 17:16:19 +0200

    first commit checking commiting system

---
+++ Excercise_1/Excercise_1/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // this is first check for github commit
+            Console.WriteLine("Hello World");
+
+
+
+            Console.ReadKey();
+            //check
+            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        }
+    }
+}
