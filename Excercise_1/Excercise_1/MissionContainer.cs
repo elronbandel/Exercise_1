15ea26a000e523b0aafdd293debb12997d376ec2
elron <elronbandel@gmail.com>
2019-03-12 17:43:50 +0200

    mission container added and debugged

--- Excercise_1/Excercise_1/MissionContainer.cs
+++ Excercise_1/Excercise_1/MissionContainer.cs
@@ -6,10 +6,45 @@ using System.Threading.Tasks;
 
 namespace Excercise_1
 {
-  
-    public class FunctionsContainer
+
+
+    class FunctionsContainer
     {
-       
+        private Dictionary<String, Func<double, double>> functions;
+
+        public FunctionsContainer()
+        {
+            functions = new Dictionary<string, Func<double, double>>();
+        }
+
+        public Func<double, double> this[string key]
+        {
+            get
+            {
+                if (!functions.ContainsKey(key))   // if it dosent contains the key consider as function does nothing
+                {
+                    functions[key] = val => val;
+                }
+                return functions[key];
+            }
+            set
+            {
+                functions[key] = value;
+            }
+
+        }
+        public string[] getAllMissions()
+        {
+            string[] names = new string[functions.Count];
+            int i = 0;
+            foreach (var entry in functions)
+            {
+                names[i++] = entry.Key;
+            }
+            return names;
+        }
+    }
+
+
 
     }
-}
